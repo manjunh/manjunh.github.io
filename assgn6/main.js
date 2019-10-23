140a95f1a71aa99b36757ecee7eae3dcdc2b92ca
manjunh <50638436+manjunh@users.noreply.github.com>
2019-10-23 10:26:18 -0400

    6A

--- assgn6/main.js
+++ assgn6/main.js
@@ -4,6 +4,7 @@ var dictionary = {
 	"Blackberry": 3.50,
 }
 
+// function that shows updates price according to the quantity users choose
 function myFunction() {
 	var x = document.getElementById("quantity").value;
 	var priceStr = document.getElementById("total").innerHTML;
@@ -11,10 +12,19 @@ function myFunction() {
   	document.getElementById("total").innerHTML = `Subtotal: $${dictionary[ori]*parseInt(x)}.00`
   }
 
+// notification alert and add 1 to the cart
 function moveFunction() {
+  let cart = 0; 
+  if (localStorage.getItem('cart')) {
+      cart = parseInt(localStorage.getItem('cart'))
+  }
+  cart += 1;
+  localStorage.setItem('cart', cart)
+  document.getElementById('cart-count').innerHTML = `${cart}`
 	alert("Successfully moved to the Cart!")
 }
 
+// changing image as users switch between radio selection
 function changeImage(){
 	var choiceInputArr = document.getElementsByClassName("choice");
 	var checkedInputTag = Array.from(choiceInputArr).filter(inputObject => inputObject.checked)
